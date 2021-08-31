c557857c3c9b6027161c2613662702a3dc552c18
Daniel Piñeros <odpinerosh@gmail.com>
2021-08-31 07:46:11 -0500

    (feat) añadí clase CDT

--- src/banco/CDT.java
+++ src/banco/CDT.java
@@ -1,15 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package banco;
 
-/**
- *
- * @author odpinerosh
- */
-public class CDT
-  {
+
+public class CDT {
+
+    private String numCDT;
+    private String fechCDT;
+    private double intCDT;
+    private double montoCDT;
+    private int plazoCDT;
     
-  }
+    public void setNumCuenta(String numeroCta) {
+        this.numCuenta = numeroCta;
+    }
+    
+    public String getNumCuenta() {
+        return this.numCuenta;
+    }
+    
+    public void setFechCuenta(String fechaCta) {
+        this.fechCuenta = fechaCta;
+    }
+    
+    public String getFechCuenta() {
+        return this.fechCuenta;
+    }
+    
+    public void setIntCuenta(double interesCta) {
+        this.intCuenta = interesCta;
+    }
+    
+    public double getIntCuenta() {
+        return this.intCuenta;
+    }
+    
+    public void setSalCuenta(double saldoCta) {
+        this.salCuenta = saldoCta;
+    }
+    
+    public double getSalCuenta() {
+        return this.salCuenta;
+    }
+
+    
+}
